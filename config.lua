de99446754726bd3f6df9bcafddb1dd675e36da5
collin.li <collin.li@nio.com>
2023-02-13 18:55:44 +0800

    feat(noice): only message error notify

--- config.lua
+++ config.lua
@@ -645,9 +645,9 @@ lvim.plugins = {
           -- NOTE: If you enable messages, then the cmdline is enabled automatically.
           -- This is a current Neovim limitation.
           enabled = true, -- enables the Noice messages UI
-          view = "mini", -- default view for messages
-          view_error = "mini", -- view for errors
-          view_warn = "mini", -- view for warnings
+          view = false, -- default view for messages
+          view_error = "notify", -- view for errors
+          view_warn = false, -- view for warnings
           view_history = "messages", -- view for :messages
           view_search = false, -- view for search count messages. Set to `false` to disable
         }, -- add any options here
@@ -682,6 +682,9 @@ lvim.plugins = {
                 Search = "",
               },
             },
+          },
+          notify = {
+            timeout = 1800,
           },
         }
       })
